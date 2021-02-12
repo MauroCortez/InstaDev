2ba7c5dc9c4f5985297a9bb164f8f09ab2301ada
FIC <FIC@info.local>
2021-02-12 18:12:10 -0200

    View arrumado

--- Controllers/CadastroController.cs
+++ Controllers/CadastroController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace InstaDev.Controllers
 {
+    [Route("Cadastro")]
     public class CadastroController : Controller
     {
         public IActionResult Index()
